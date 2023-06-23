7c66d83ad7853154262640412246e0e525b33708
Elijah CIali <elijahciali@icloud.com>
2023-06-23 11:11:31 -0500

    Initial Commit

---
+++ CloudClear/CloudClearApp.swift
@@ -0,0 +1,17 @@
+//
+//  CloudClearApp.swift
+//  CloudClear
+//
+//  Created by Elijah Ciali on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct CloudClearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
